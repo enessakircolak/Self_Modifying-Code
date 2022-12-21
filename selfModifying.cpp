7db3571dfefecbc134c3c0ed16d576ce11c950dd
enessakircolak <69612461+enessakircolak@users.noreply.github.com>
2022-12-22 01:57:13 +0300

    Update selfModifying.cpp

--- selfModifying.cpp
+++ selfModifying.cpp
@@ -32,28 +32,7 @@ int main()
     }*/
     return 0;
 }
-/*int number = 0, times = 0, result = 0;
-    cout << "input : "<< endl;
-    cin >> number;
-    cout << "input times: " << endl;
-    cin >> times;
 
 
-    for (int i = 0; i < times; i++)
-        result += number;
-
-    cout << "Final : " << result << endl;*/
-
-
-// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
-// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
-
-// Kullanmaya Başlama İpuçları: 
-//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
-//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
-//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
-//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
-//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
-//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
-
+ 
 
